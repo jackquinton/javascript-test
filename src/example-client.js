7fe66490e0d8b4a766e64c3884027ecd7f1cc4f5
jackquinton <jackquintonaudio@gmail.com>
2024-12-19 19:49:08 +0000

    Call these methods in the example client

--- src/example-client.js
+++ src/example-client.js
@@ -1,4 +1,24 @@
-const BookSearchApiClient = require("./BookSearchApiClient.js");
+const BookSearchApiClient = require("./BookSearchApiClient.ts");
 
-const client = BookSearchApiClient();
-const booksByShakespeare = client.getBooksByAuthor("Shakespeare", 10);
+const client = new BookSearchApiClient();
+
+async function searchBooks() {
+  try {
+    // Search by author
+    const shakespeareBooks = await client.getBooksByAuthor("Shakespeare", 10);
+    
+    // Search by publisher
+    const penguinBooks = await client.getBooksByPublisher("Penguin Books", 5);
+    
+    // Search by year
+    const books2023 = await client.getBooksByYear(2023, 20);
+    
+    // Search by ISBN
+    const specificBook = await client.getBooksByIsbn("1234567890");
+    
+  } catch (error) {
+    console.error("Error searching books:", error);
+  }
+}
+
+searchBooks();
